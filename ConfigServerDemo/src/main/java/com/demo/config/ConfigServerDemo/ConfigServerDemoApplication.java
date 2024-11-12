a8968910b3b107687848dbb427fe0f3f53126ecd
Shalini Mittal <shalini06.mittal@gmail.com>
2024-11-12 10:15:24 +0530

    cloud config

---
+++ ConfigServerDemo/src/main/java/com/demo/config/ConfigServerDemo/ConfigServerDemoApplication.java
@@ -0,0 +1,16 @@
+package com.demo.config.ConfigServerDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerDemoApplication.class, args);
+	}
+
+}
