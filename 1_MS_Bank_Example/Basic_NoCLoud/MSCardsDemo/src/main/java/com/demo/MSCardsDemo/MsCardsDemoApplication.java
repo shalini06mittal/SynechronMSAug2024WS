3a5057731a1479b79ecf513eab7b87f3184e13bb
Shalini Mittal <shalini06.mittal@gmail.com>
2024-11-16 11:28:05 +0530

    updated loans aand card for auditing

--- 1_MS_Bank_Example/Basic_NoCLoud/MSCardsDemo/src/main/java/com/demo/MSCardsDemo/MsCardsDemoApplication.java
+++ 1_MS_Bank_Example/Basic_NoCLoud/MSCardsDemo/src/main/java/com/demo/MSCardsDemo/MsCardsDemoApplication.java
@@ -2,8 +2,10 @@ package com.demo.MSCardsDemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
 public class MsCardsDemoApplication {
 
 	public static void main(String[] args) {
--- 1_MS_Bank_Example/Basic_NoCLoud/MSLoansDemo/src/main/java/com/demo/MSLoansDemo/MsLoansDemoApplication.java
+++ 1_MS_Bank_Example/Basic_NoCLoud/MSLoansDemo/src/main/java/com/demo/MSLoansDemo/MsLoansDemoApplication.java
@@ -2,8 +2,10 @@ package com.demo.MSLoansDemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
 public class MsLoansDemoApplication {
 
 	public static void main(String[] args) {
